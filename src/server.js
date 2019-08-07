21f4fb73066b1346b4c036ba380136e56191e333
dsausen <53322492+dsausen@users.noreply.github.com>
2019-08-06 21:29:21 -0300

    Add CORS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require("express"),
 mongoose = require("mongoose"),
 routes = require("./routes"),
+cors = require("cors"),
 server = express()
 
 mongoose.connect("mongodb+srv://dannystack:dannystack@cluster0-anp2r.mongodb.net/omnistack8?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 
