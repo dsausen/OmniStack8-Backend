82b17e8166d1c055be6489b5c29bc0ad20f549ef
dsausen <53322492+dsausen@users.noreply.github.com>
2019-09-01 22:27:15 -0300

    Final touches

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,26 +1,37 @@
-const dev = require("../models/dev")
+const dev = require("../models/dev");
 
 module.exports = {
-    async store(req, res) {
-        const { user } = req.headers, //loggedDev
-        { devId } = req.params, //targetDev
-        loggedDev = await dev.findById(user),
-        targetDev = await dev.findById(devId)
+  async store(req, res) {
+    console.log(req.io, req.connectedUsers);
 
-        //If devId does not exist, return status 400
-        if(!targetDev) {
-            return res.status(400).json({ error: "Dev does not exist" })
-        }
+    const { user } = req.headers, //loggedDev
+      { devId } = req.params, //targetDev
+      loggedDev = await dev.findById(user),
+      targetDev = await dev.findById(devId);
 
-        //If devId has also liked user, print match message
-        if(targetDev.likes.includes(loggedDev._id)) {
-            console.log("DEU MATCH!")
-        }
-
-        //Pushes like to database
-        loggedDev.likes.push(targetDev._id)
-        await loggedDev.save()
-
-        return res.json(loggedDev)
+    //If devId does not exist, return status 400
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev does not exist" });
     }
-}
+
+    //If devId has also liked user, print match message
+    if (targetDev.likes.includes(loggedDev._id)) {
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit("match", targetDev);
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit("match", loggedDev);
+      }
+    }
+
+    //Pushes like to database
+    loggedDev.likes.push(targetDev._id);
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
